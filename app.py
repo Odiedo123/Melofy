1b83a818a857b8737f96efd25094ddbb00ac03cd
Jed Oloo <odiedo123@gmail.com>
2025-06-29 11:01:01 +0300

    Basic home page

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, send_from_directory, jsonify, render_template, redirect, url_for, session, Response, make_response, send_file
+
+app = Flask(__name__)
+
+@app.route("/")
+def home_page():
+    return render_template('index.html')
+
+@app.route("/signup")
+def signup_page():
+    return render_template('signup.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
